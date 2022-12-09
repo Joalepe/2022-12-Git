ca095dc5dc3ef3a4635bf83fdd2d23bdb2d12eab
Joalepe <jouni.pehkonen@student.hamk.fi>
2022-12-09 07:04:39 +0200

    Versio 1
    Luotu auto-luokka, joka sisältää perustiedot

--- src/GitTehtava1.java
+++ src/GitTehtava1.java
@@ -2,8 +2,41 @@
 public class GitTehtava1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+
+		
 	}
 
+}
+class Auto {
+	
+	public String merkki;
+	
+	public String malli;
+	
+	public int bensanMaara;
+	
+	public Auto()
+	{
+		merkki="";
+		malli="";
+		bensanMaara=0;
+	}
+	
+	public Auto(String autonMerkki, String autonMalli)
+	{
+		merkki=autonMerkki;
+		malli=autonMalli;
+		bensanMaara=0;
+	}
+	
+
+	
+	public void naytaTiedot()
+	{
+		System.out.println("Merkki: " + merkki);
+		System.out.println("Malli: " + malli);
+		System.out.println("Tankissa bensaa: " + bensanMaara);
+	}
 }
